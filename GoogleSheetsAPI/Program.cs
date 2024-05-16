7e142f0981a84bba2e6b154cc36ce9c169dbdb42
Andrew Gunn <andrewgunn31@gmail.com>
2024-05-16 12:40:15 -0500

    Refactor API endpoints, add write, read, update & delete functions
    Added new API endpoints for writing, reading, updating, and deleting data in Google Sheets using specified ranges. Refactored existing code to accommodate these changes.

--- GoogleSheetsAPI/Program.cs
+++ GoogleSheetsAPI/Program.cs
@@ -94,16 +94,12 @@ app.UseCors("Local");
 // app.UseHttpsRedirection();
 
 // Define endpoints here.
-app.MapPost("/api/write", async (SheetsService sheetsService, [FromBody] WriteRequestDto dto) =>
+app.MapPost("api/write", async (SheetsService sheetsService, [FromBody] WriteRequestDto dto) =>
 {
-    // string spreadsheetId2 = "1IETU7EI1UKkVGgaCcoz0R0cnX5tdme-6ealsXvtXR1k";
-    // string range2 = "Sheet1!A1:D1";
     var fullRange = $"{dto.Sheetname ?? "Sheet1"}!{dto.Range ?? "A1"}";
-
     var objList = dto.Values.Select(GetObjectValue).ToList();
 
     var valueRange = new Google.Apis.Sheets.v4.Data.ValueRange() { Values = new List<IList<object>> { objList } };
-
     var request = sheetsService.Spreadsheets.Values.Update(valueRange, dto.SpreadsheetId, fullRange);
 
     request.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
@@ -111,14 +107,36 @@ app.MapPost("/api/write", async (SheetsService sheetsService, [FromBody] WriteRe
     return Results.Ok(response.UpdatedRange);
 }).WithName("WriteData").WithOpenApi();
 
-app.MapGet("/api/read", async (SheetsService sheetsService) =>
+app.MapGet("api/read", async (SheetsService sheetsService, [FromBody] WriteRequestDto dto) =>
 {
-    string spreadsheetId = "1IETU7EI1UKkVGgaCcoz0R0cnX5tdme-6ealsXvtXR1k";
-    string range = "Sheet1!A1:D1";
-    var request = sheetsService.Spreadsheets.Values.Get(spreadsheetId, range);
+    var fullRange = $"{dto.Sheetname ?? "Sheet1"}!{dto.Range ?? "A1"}";
+    var request = sheetsService.Spreadsheets.Values.Get(dto.SpreadsheetId, fullRange);
+
     var response = await request.ExecuteAsync();
     return Results.Ok(response.Values);
 }).WithName("ReadData").WithOpenApi();
+
+app.MapPut("api/update", async (SheetsService sheetsService, [FromBody] WriteRequestDto dto) =>
+{
+    var fullRange = $"{dto.Sheetname ?? "Sheet1"}!{dto.Range ?? "A1:Z1"}";
+    var objList = dto.Values.Select(GetObjectValue).ToList();
+
+    var valueRange = new Google.Apis.Sheets.v4.Data.ValueRange() { Values = new List<IList<object>> { objList } };
+    var request = sheetsService.Spreadsheets.Values.Update(valueRange, dto.SpreadsheetId, fullRange);
+
+    request.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
+    var response = await request.ExecuteAsync();
+    return Results.Ok(response.UpdatedRange);
+}).WithName("UpdateData").WithOpenApi();
+
+app.MapDelete("api/delete", async (SheetsService sheetsService, [FromBody] WriteRequestDto dto) =>
+{
+    var fullRange = $"{dto.Sheetname ?? "Sheet1"}!{dto.Range ?? "A1:Z1"}";
+    var request = sheetsService.Spreadsheets.Values.Clear(null, dto.SpreadsheetId, fullRange);
+
+    var response = await request.ExecuteAsync();
+    return Results.Ok(response.ClearedRange);
+}).WithName("DeleteData").WithOpenApi();
 
 
 app.Run();
